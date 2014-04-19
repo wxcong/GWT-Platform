937054ccdbbb158438316fb4dfb33074711f96f8
andy wang <wxcnyu2014@gmail.com>
2014-04-19 15:25:04 -0400

    Version 1.0

---
+++ src/org/client/container/ContainerEntryPoint.java
@@ -0,0 +1,12 @@
+package org.client.container;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ContainerEntryPoint implements EntryPoint{
+
+  @Override
+  public void onModuleLoad() {
+	  
+	  
+  }
+}
